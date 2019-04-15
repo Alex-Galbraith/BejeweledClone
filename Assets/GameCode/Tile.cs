d019cf4edd8bdb2bc7e6edcbfe98e4edbcbbc95e
Alex_Galbraith <alexander.sr.galbraith@gmail.com>
2019-04-15 20:26:58 +1200

    Added basic tile info

--- Assets/GameCode/Tile.cs
+++ Assets/GameCode/Tile.cs
@@ -2,17 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace TSwapper { 
+    /// <summary>
+    /// Tile superclass. Since this is a simple project, all of the tile
+    /// data and logic is stored here.
+    /// </summary>
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
     {
-        
+        public TileType tileType;
+        /// <summary>
+        /// Is this a basic matching tile, or do we need to treat it with
+        /// greater care. E.G., do we need to call PostFlip
+        /// </summary>
+        [Tooltip("Is this a basic matching tile, or do we need to treat it with greater care?")]
+        public bool isComplex = false;
+
+        /// <summary>
+        /// This particle will be automatically triggered on death.
+        /// </summary>
+        [Tooltip("This particle will be automatically triggered on death.")]
+        public ParticleSystem onDeathParticle;
+
+        /// <summary>
+        /// How much is destroying this tile worth?
+        /// </summary>
+        [Tooltip("How much is destroying this tile worth?")]
+        public int baseScoreValue;
+
+        SpriteRenderer spriteRenderer;
+
+        /// <summary>
+        /// Only called if isComplex is true. Use this function to perform special actions after
+        /// this tile has been flipped. We could use events for this, but it would be a little 
+        /// over the top for this project.
+        /// </summary>
+        public void PostFlip() {
+
+        }
+
+        /// <summary>
+        /// Only called if isComplex is true. Use this function to perform special actions after
+        /// a neighbouring tile has been flipped. We could use events for this, but it would be a little 
+        /// over the top for this project.
+        /// </summary>
+        public void NeighbourFlipped() {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
---
+++ Assets/GameCode/TileEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSwapper {
+    public enum TileType {
+        EMPTY,
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        PURPLE,
+        BOMB,
+        ANY
+    }
+}
