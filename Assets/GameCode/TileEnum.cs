1cd934d9ea78f2cad2c56ee28977be630c7205a3
Alex_Galbraith <alexander.sr.galbraith@gmail.com>
2019-04-17 13:12:32 +1200

    Added gem types

--- Assets/GameCode/TileEnum.cs
+++ Assets/GameCode/TileEnum.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 namespace TSwapper {
     [System.Flags]
     public enum TileType {
-        EMPTY   = 0,
+        SPECIAL = 0,
         RED     = 1,
         GREEN   = 2,
         BLUE    = 4,
         YELLOW  = 8,
         PURPLE  = 16,
-        BOMB    = 32,
+        WHITE   = 32,
+        MAGENTA = 64,
     }
 
     public enum PoolGroup {
-        BASIC
+        BASIC,
+        BOMB
     }
 }
