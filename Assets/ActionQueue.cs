135a834322d6382dee1a4c2da5342e02adfd3b23
Alex_Galbraith <alexander.sr.galbraith@gmail.com>
2019-04-17 19:57:33 +1200

    Queue can now have items pushed to the head

--- Assets/ActionQueue.cs
+++ Assets/ActionQueue.cs
@@ -3,34 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace TSwapper { 
+    /// <summary>
+    /// Queues and exectutes actions.
+    /// </summary>
     public class ActionQueue : MonoBehaviour
     {
-        private Queue<Action> Actions = new Queue<Action>();
+        private LinkedList<Action> Actions = new LinkedList<Action>();
         private bool ActionInProgress;
         private Action active;
         int ID = 0;
+
         // Update is called once per frame
         void Update()
         {
+            //Dequeue actions and execute them.
             if (!ActionInProgress) {
                 if (Actions.Count > 0) {
-                    active = Actions.Dequeue();
+                    active = Actions.First.Value;
+                    Actions.RemoveFirst();
                     ActionInProgress = true;
                     active.Invoke(++ID);
                     
                 }
             }
         }
-
+        /// <summary>
+        /// Callback for actions.
+        /// </summary>
+        /// <param name="id">ID given to the action when it was called.</param>
         public void ActionComplete(int id) {
             if (id == ID){ 
                 ActionInProgress = false;
                 active = null;
             }
+            else {
+                Debug.LogWarning("Incorrect action ID processed. found: " + id + " Expected: " + ID);
+            }
+        }
+        /// <summary>
+        /// Add an action ot the end of the queue.
+        /// </summary>
+        public void Enqueue(Action action) {
+            Actions.AddLast(action);
         }
 
-        public void Enqueue(Action action) {
-            Actions.Enqueue(action);
+        /// <summary>
+        /// Add am action to the start of the queue.
+        /// </summary>
+        /// <param name="action"></param>
+        public void JumpQueue(Action action) {
+            Actions.AddFirst(action);
         }
     }
 
