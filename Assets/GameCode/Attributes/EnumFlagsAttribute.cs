09b812a7469a783544689752fc89a01d51536be3
Alex_Galbraith <alexander.sr.galbraith@gmail.com>
2019-04-17 14:51:17 +1200

    Removed attribution as I have now reewriteen the important bits myself

--- Assets/GameCode/Attributes/EnumFlagsAttribute.cs
+++ Assets/GameCode/Attributes/EnumFlagsAttribute.cs
@@ -1,13 +1,4 @@
-﻿
-/*
- * 
- * This file contains an attribute for drawing in editor bitmasks.
- * The code is from: https://answers.unity.com/questions/486694/default-editor-enum-as-flags-.html
- * 
- */
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnumFlagsAttribute : PropertyAttribute {
     public EnumFlagsAttribute() { }
