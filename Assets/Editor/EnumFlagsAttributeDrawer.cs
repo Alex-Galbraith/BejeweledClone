38683c3caa37ca9cb3fa0158d419f76182fb418f
Alex_Galbraith <alexander.sr.galbraith@gmail.com>
2019-04-21 15:29:16 +1200

    Fixed multi editing issue

--- Assets/Editor/EnumFlagsAttributeDrawer.cs
+++ Assets/Editor/EnumFlagsAttributeDrawer.cs
@@ -6,6 +6,10 @@ using System;
 [CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
 public class EnumFlagsAttributeDrawer : PropertyDrawer {
     public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label) {
+        if (_property.serializedObject.isEditingMultipleObjects) {
+            EditorGUI.LabelField(_position, _label);
+            return;
+        }
         Enum targetEnum = (Enum)Enum.ToObject(fieldInfo.FieldType, _property.intValue);
         Enum result =  EditorGUI.EnumFlagsField(_position, _label, targetEnum);
         _property.intValue = (int)Convert.ChangeType(result, targetEnum.GetType());
