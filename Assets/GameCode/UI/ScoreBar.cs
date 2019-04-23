08f622d245307f0d0d16ef2cf143f08e066ebe73
Alex_Galbraith <alexander.sr.galbraith@gmail.com>
2019-04-23 17:42:53 +1200

    Updated UI

--- Assets/GameCode/UI/ScoreBar.cs
+++ Assets/GameCode/UI/ScoreBar.cs
@@ -10,8 +10,18 @@ namespace TSwapper {
         public IntReference maxScoreRef;
 
         private void OnChange(int before, int after) {
-            float frac = Mathf.Min(1,((float)after) / (float)maxScoreRef.Value);
-            front.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, frac * back.rect.width);
+            StartCoroutine(TweenScore(0.5f, before, after));
+        }
+
+        private IEnumerator TweenScore(float time, int before, int after) {
+            float cTime = 0;
+            while (cTime < time) {
+                cTime += Time.deltaTime;
+                float frac = Mathf.Min(1, Mathf.Lerp(before, after, cTime / time) / (float)maxScoreRef.Value);
+                front.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, frac * back.rect.width);
+                yield return null;
+            }
+
         }
 
         private void OnChangeMax(int before, int after) {
---
+++ Assets/GameCode/UI/ScoreText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TSwapper {
+    [ExecuteInEditMode, RequireComponent(typeof(Text))]
+    public class ScoreText : MonoBehaviour {
+        public IntReference scoreRef, maxScoreRef;
+        public Text toUpdate;
+        public string divider = " / ";
+
+        private void Awake() {
+            if (toUpdate == null)
+                toUpdate = GetComponent<Text>();
+            if (scoreRef != null) {
+                scoreRef.NotifyChange += OnScoreChange;
+            }
+        }
+
+        private void OnDestroy() {
+            scoreRef.NotifyChange -= OnScoreChange;
+        }
+
+        private void OnScoreChange(int before, int after) {
+            StartCoroutine(TweenScore(0.5f, before, after));
+        }
+
+        private IEnumerator TweenScore(float time, int before, int after) {
+            float cTime = 0;
+            while (cTime < time) {
+                cTime += Time.deltaTime;
+                toUpdate.text = Mathf.CeilToInt(Mathf.Lerp(before,after,cTime/time)) + divider + maxScoreRef.Value;
+                yield return null;
+            }
+            
+        }
+    }
+}
