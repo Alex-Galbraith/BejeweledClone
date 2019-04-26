1011c0ae1ee141a60243b245f1c64f3080a8b6f4
Alex_Galbraith <alexander.sr.galbraith@gmail.com>
2019-04-26 14:48:21 +1200

    Moved MaterialIcons and added quit button

---
+++ Assets/GameCode/UI/Quit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace TSwapper.UI { 
+    public class Quit : MonoBehaviour {
+        public void DoQuit() {
+            Application.Quit();
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+    }
+}
