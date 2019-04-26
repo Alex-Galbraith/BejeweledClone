01069b51ec4a67b0ff07cfad89434cea0cd13285
Alex_Galbraith <alexander.sr.galbraith@gmail.com>
2019-04-26 23:21:43 +1200

    Added comment

--- Assets/GameCode/TileFacade.cs
+++ Assets/GameCode/TileFacade.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 namespace TSwapper {
+    /// <summary>
+    /// Fake tile used for special effects
+    /// </summary>
     [RequireComponent(typeof(SpriteRenderer))]
     public class TileFacade : MonoBehaviour {
         public SpriteRenderer spriteRenderer;
